67110277f1a611749ff50bb13d9b932be808d7d2
ramin farajpour cami <ramin.blackhat@gmail.com>
2017-01-17 12:30:25 +0330

    fixed sending file linux to windows file sharing

--- copyfile_linux_to_windows.sh
+++ copyfile_linux_to_windows.sh
@@ -1,2 +1,2 @@
 filename=`ls -ltr | grep ^- | tail -1 | awk '{ print $(NF) }'` ## get last file added or modify
-smbclient -U <username>%'<pass>' //<هح>/backup --directory \<directory_name>  -c "put "${filename}"" ## send to windows file sharing
+smbclient -U <username>%'<pass>' //<IP address>/backup --directory \<directory_name>  -c "put "${filename}"" ## send to windows file sharing
