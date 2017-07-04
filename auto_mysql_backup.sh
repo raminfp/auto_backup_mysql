35dcc1d995f803fd4ebdc7144ca47b9b05899ccb
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2017-07-04 08:19:51 +0430

    Fixed compress database

--- auto_mysql_backup.sh
+++ auto_mysql_backup.sh
@@ -7,26 +7,17 @@
 dblist="DB1 DB2"
 
 backupdir=/root/
-
-
 hotcopycmd="/usr/bin/mysqldump --lock-tables --databases"
-
 echo "Dumping MySQL Databases..."
-RC=0
-
+echo "##########################"
 for database in $dblist; do
 
         backuppath=/root/$database
         mkdir -p "$backuppath"
         $hotcopycmd $database > "$backuppath"/"$database""_"$(date +%Y%m%d%H%M%S).sql
-		echo "Dumping $database ..."
+	$hotcopycmd $database | gzip > "$backuppath"/"$database""_"$(date +%Y%m%d%H%M%S).sql.gz
+	echo "Dumping $database ..."
+	
 done
 
-if [ $RC -gt 0 ]; then
-   echo "MySQL Dump failed!"
-   exit $RC
-else
-   ls -l "$backupdir"
-   echo "MySQL Dump is complete!"
-fi
 exit 0
